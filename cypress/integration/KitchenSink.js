07e45048c2f9a55c5069e0f38c5fe680dbb260c5
Josiah Maina <josiah5115@gmail.com>
2018-10-19 12:12:57 -0600

    Email Test

--- cypress/integration/KitchenSink.js
+++ cypress/integration/KitchenSink.js
@@ -2,5 +2,12 @@
 describe('can visit site and cick things', () => {
   it('can visit the site', () => {
    cy.visit('https://example.cypress.io')
+   cy.title().should('include', 'Kitchen Sink')
+  })
+
+  it('can click a link', () => {
+   cy.get('a').contains('type').click();
+   cy.get('#email1').type('rasjoh@gmail.com{enter}');
+   cy.get('#email1').should('have.value', '');
   })
 });
